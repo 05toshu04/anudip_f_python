fd081b6af2d3dd92890f5b1143356607d02ade24
tushar goswami <tussygoswami01@gmail.com>
2026-03-31 10:30:14 +0530

    second largest element from a list without using built-in sort

---
+++ python_assessment/second_largest_num.py
@@ -0,0 +1,19 @@
+def second_largest(arr):
+    if len(arr) < 2:
+        return None  # Not enough elements
+
+    first = second = float('-inf')
+
+    for num in arr:
+        if num > first:
+            second = first
+            first = num
+        elif num > second and num != first:
+            second = num
+
+    return second
+
+
+# Example
+arr = [10, 5, 8, 20, 15]
+print("Second largest:", second_largest(arr))
